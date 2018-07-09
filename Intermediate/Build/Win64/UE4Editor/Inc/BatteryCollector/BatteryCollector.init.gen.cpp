541759066a5eb851b3db2cfa98ba1c21c816c6f7
Martin Fischer <martin.fischer@amt-essen.com>
2018-07-09 12:35:55 +0200

    C++ Battery Collector: Making Your First Pickup Class | 03 | v4.9 Tutorial Series | Unreal Engine https://youtu.be/EIptJ0YrYg0?list=PLZlv_N0_O1gYup-gvJtMsgJqnEB_dGiM4&t=184

--- Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollector.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollector.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBatteryCollector_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BatteryCollector",
 				PKG_CompiledIn | 0x00000000,
-				0x12968FDE,
-				0x3D95CCFB,
+				0x36DC67B7,
+				0x7F197C69,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/Pickup.gen.cpp
@@ -0,0 +1,67 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "Pickup.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodePickup() {}
+// Cross Module References
+	BATTERYCOLLECTOR_API UClass* Z_Construct_UClass_APickup_NoRegister();
+	BATTERYCOLLECTOR_API UClass* Z_Construct_UClass_APickup();
+	ENGINE_API UClass* Z_Construct_UClass_AActor();
+	UPackage* Z_Construct_UPackage__Script_BatteryCollector();
+// End Cross Module References
+	void APickup::StaticRegisterNativesAPickup()
+	{
+	}
+	UClass* Z_Construct_UClass_APickup_NoRegister()
+	{
+		return APickup::StaticClass();
+	}
+	UClass* Z_Construct_UClass_APickup()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_AActor,
+				(UObject* (*)())Z_Construct_UPackage__Script_BatteryCollector,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "IncludePath", "Pickup.h" },
+				{ "ModuleRelativePath", "Pickup.h" },
+			};
+#endif
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<APickup>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&APickup::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00900080u,
+				nullptr, 0,
+				nullptr, 0,
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(APickup, 3503245789);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_APickup(Z_Construct_UClass_APickup, &APickup::StaticClass, TEXT("/Script/BatteryCollector"), TEXT("APickup"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(APickup);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
