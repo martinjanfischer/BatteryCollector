520cbc9655cf98a4858f8fb873641204b7ec85a9
Martin Fischer <martin.fischer@amt-essen.com>
2018-07-16 14:56:43 +0200

    C++ Battery Collector: Powering Down the Character | 13 | v4.9 Tutorial Series | Unreal Engine https://www.youtube.com/watch?v=qCrIPglRY6M&list=PLZlv_N0_O1gYup-gvJtMsgJqnEB_dGiM4&index=13
    Finished
    
    Decrease does not work yet, as can be observed in the unreal editor.

--- Source/BatteryCollector/BatteryCollectorGameMode.h
+++ Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -13,7 +13,11 @@ class ABatteryCollectorGameMode : public AGameModeBase
 
 public:
 	ABatteryCollectorGameMode();
+	
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	/** The rate at which the character loses power */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power")
+	float DecayRate;
 };
-
-
-
