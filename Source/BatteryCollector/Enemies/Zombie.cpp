496f74ef70ffe4edc322a453c4ee6931bed523f7
Martin Fischer <martin.fischer@amt-essen.com>
2018-07-26 12:12:48 +0200

    removed inactive code

--- Source/BatteryCollector/Enemies/Zombie.cpp
+++ Source/BatteryCollector/Enemies/Zombie.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-//#include "Engine/EngineTypes.h"
 #include "BatteryCollector.h"
 #include "BatteryCollectorCharacter.h"
 #include "Zombie.h"
