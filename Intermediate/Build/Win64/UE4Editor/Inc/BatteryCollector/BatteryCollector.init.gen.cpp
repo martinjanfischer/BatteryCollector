646ffd922ede1fb8e7ed0dd1c9c764d26e8dd350
Martin Fischer <martin.fischer@amt-essen.com>
2018-07-27 11:15:47 +0200

    Revert "C++ Tanks vs Zombies | 04 | Live Training | Unreal Engine https://www.youtube.com/watch?v=vi3ZRD5VcgI&index=4&list=PLZlv_N0_O1gaz3ydgU5wt6c_JtJzwXUKW"
    This reverts commits
    10835dcd355c820823b57db42dc1361541e2865a
    28c8b1c2745fe7cd59be1d9b624508150cd702e0
    66a97f7492618e6c67332f4dbceeb5476955fbd4
    496f74ef70ffe4edc322a453c4ee6931bed523f7
    c914a95ad3fe976f7e738948f1a92bad4c79599c

--- Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollector.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollector.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBatteryCollector_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BatteryCollector",
 				PKG_CompiledIn | 0x00000000,
-				0x19F6B202,
-				0xA8432452,
+				0xB0CB1939,
+				0x3DD92963,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
