0352e97f1b7d1613f12bd2e3515bdac07b24d6d7
Martin Fischer <martin.fischer@amt-essen.com>
2018-07-27 12:16:53 +0200

    C++ Tanks vs Zombies | 04 | Live Training | Unreal Engine https://www.youtube.com/watch?v=vi3ZRD5VcgI&index=4&list=PLZlv_N0_O1gaz3ydgU5wt6c_JtJzwXUKW
    Making Zombies
    00:14:47 https://youtu.be/vi3ZRD5VcgI?list=PLZlv_N0_O1gaz3ydgU5wt6c_JtJzwXUKW&t=887
    to
    00:31:40 https://youtu.be/vi3ZRD5VcgI?list=PLZlv_N0_O1gaz3ydgU5wt6c_JtJzwXUKW&t=1902
    
    DOES NOT COMPILE YET!
    I have just introduced the Zombie.h/.cpp code from the tutorial video in a straightforward manner, without replacing the references to the Tank type, that does not exist in this demo, against our BatteryCollector Character type.
    
    I will try use the the Mannequin rig as Zombie in the future revisions.

--- Source/BatteryCollector/Enemies/Zombie.cpp
+++ Source/BatteryCollector/Enemies/Zombie.cpp
@@ -1,5 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Tanks.h"
+#include "Tank.h"
 #include "Zombie.h"
 
 
@@ -9,6 +11,17 @@ AZombie::AZombie()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	
+	Health = 100.0f;
+	SightDistance = 500.0f;
+	SightAngle = 60.0f;
+	YawSpeed = 90.0f;
+	WalkSpeed = 25.0f;
+	RunSpeed = 45.0f;
+	AttackDistance = 100.0f;
+	AttackAngle = 30.0f;
+	AttackCooldown = 1.0f;
 }
 
 // Called when the game starts or when spawned
@@ -32,3 +45,18 @@ void AZombie::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
+void AZombie::SetTarget(AActor* NewTarget)
+{
+	TargetActor = NewTarget;
+	TargetTank = Cast<ATank>(NewTarget);
+}
+
+AActor* AZombie::GetTarget()
+{
+	return TargetActor;
+}
+
+ATank* AZombie::GetTargetAsTank()
+{
+	return TargetTank;
+}
--- Source/BatteryCollector/Enemies/Zombie.h
+++ Source/BatteryCollector/Enemies/Zombie.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "Zombie.generated.h"
 
+class ATank;
+
 UCLASS()
 class BATTERYCOLLECTOR_API AZombie : public ACharacter
 {
@@ -26,6 +28,69 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// Set this zombie's target. The base version of this function will handle updating TargetActor and T .
+	UFUNCTION(BlueprintCallable, Category = "AI")
+	void SetTarget(AActor* NewTarget);
+
+	// Return the target Actor. Returning NULL indicates no target.
+	UFUNCTION(BlueprintCallable, Category = "AI")
+	AActor* GetTarget();
+
+	// Return the target Actor as a Tank, if possible. Returning NULL indicates no target, or that the tank .
+	UFUNCTION(BlueprintCallable, Category = "AI")
+	ATank* GetTargetAsTank();
 	
+private:
+	/** The actor we're targeting. Will be NULL if there is not target. */
+	UPROPERTY(VisibleInstanceOnly, Category = "AI")
+	AActor* TargetActor;
+
+	/** The actor we're targeting, cast to a Tank and cached. Will be NULL if no target if the target a . */
+	UPROPERTY(VisibleInstanceOnly, Category = "AI")
+	ATank* TargetTank;
 	
+protected:
+	/** Camera effect, if any, to play when the player is hit by this Zombie. */
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UCameraShake> HitShake;
+	
+	/** Current health value. Might be fun to have different values for different attack types, e.g. fire . */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float Health;
+
+	/** Sight distance (when not target is present). */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float SightDistance;
+
+	/** Sight half-angle in degrees (when not target is present). */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float SightAngle;
+
+	/** Max yaw speed per second in degrees. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float YawSpeed;
+
+	/** Speed when wandering around, or when facing away from the target. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float WalkSpeed;
+
+	/** Speed when running toward target. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float RunSpeed;
+
+	/** Attack distance. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float AttackDistance;
+
+	/** Half-angle (in degrees) for our attack. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float AttackAngle;
+
+	/** Minimum time between attacks. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float AttackCooldown;
+	
+	/** Game time, in seconds, when the Zombie will be allowed to attack again. */
+	UPROPERTY(Transient, VisibleInstanceOnly, BlueprintReadWrite, Category = "Zombie")
+	float AttackAvailableTime;
 };
